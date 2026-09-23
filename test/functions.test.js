ddcccb46698df05aafc98c487f2a9bb896576b53
David Torro <davidtorro.works@gmail.com>
2026-09-23 13:05:04 +0200

    feat(tests): agregar pruebas para getBookIndexById en functions.test.js

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest'
 import data from '../src/data/datos.js'
-import { getBookById } from '../src/functions.js'
+import { getBookById, getBookIndexById } from '../src/functions.js'
 
 describe('getBookById', () => {
   test('Devuelve el libro cuya id coincide', () => {
@@ -11,5 +11,19 @@ describe('getBookById', () => {
 
   test('Lanza un error si no existe el libro', () => {
     expect(() => getBookById(data.books, 999)).toThrow('Libro no encontrado')
+  })
+})
+
+describe('getBookIndexById', () => {
+  test('Devuelve la posición del libro cuya id coincide', () => {
+    const bookIndex = getBookIndexById(data.books, 6)
+
+    expect(bookIndex).toBe(1)
+  })
+
+  test('Lanza un error si no existe el libro', () => {
+    expect(() => getBookIndexById(data.books, 999)).toThrow(
+      'Libro no encontrado'
+    )
   })
 })
